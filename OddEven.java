fd97f4aa2eada3ca685224e8917b8372f43419e0
Andrea Betrina P <andreabetrina06@gmail.com>
2025-12-15 00:17:00 +0530

    Add EvenOdd class to determine even or odd numbers
    Implement a program to check if a number is even or odd.

---
+++ OddEven.java
@@ -0,0 +1,11 @@
+//program to print whether a number is even or odd.
+
+class EvenOdd {
+public static void main(String[] args) {
+        int num=1026;
+        if (num % 2 == 0)
+          System.out.println("Even Number");
+        else
+           System.out.println("Odd Number");
+    }
+}
