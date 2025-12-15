261284fe03fc54b3b52e4a6ac26da77fc2cfb054
Andrea Betrina P <andreabetrina06@gmail.com>
2025-12-15 10:36:24 -0800

    Create GrettingMessage.java

---
+++ GrettingMessage.java
@@ -0,0 +1,8 @@
+#Gretting Message
+class GreetingMessage {
+    public static void main(String[] args) {
+        String name = "Andreaaa";   
+
+        System.out.println(name + ", I'm a member of Belmontiess!!!");
+    }
+}
