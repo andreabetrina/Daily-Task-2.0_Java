00cce5ebd3ebf6b8f098a21d1379832143e4e7cd
Andrea Betrina P <andreabetrina06@gmail.com>
2025-12-16 23:56:31 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,13 @@
+// Simple Interest 
+class SInterest{
+public static void main( String [] args)
+{
+int SI;
+int P = 5000;
+int N = 3;
+int R = 5;
+SI =  (P*N*R)/100;
+System.out.println("Simple Interest is "+SI);
+}
+}
+
