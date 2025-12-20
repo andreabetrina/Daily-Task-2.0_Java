92b40260f86ce069b908abb8b6573ee6c751ef34
Andrea Betrina P <andreabetrina06@gmail.com>
2025-12-20 22:13:23 +0530

    Create FibonacciiSeries.java

---
+++ FibonacciiSeries.java
@@ -0,0 +1,14 @@
+// to generate the Fibonacci series
+class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0, b = 1, c;
+        int n = 1005;   // number of termssss
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
