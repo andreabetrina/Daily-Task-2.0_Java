383d9286948a614aa4a1e655d3dfb2b02180ca42
Andrea Betrina P <andreabetrina06@gmail.com>
2026-01-04 23:26:55 +0530

    Implement string reversal program in Day20.java
    Added a Java program to reverse a given string with an example.

---
+++ Day20.java
@@ -0,0 +1,32 @@
+Day - 20:
+Write a Java program to reverse a given string.
+
+Example
+Input: HELLO
+
+Output: OLLEH
+
+Explanation
+
+Original string: HELLO
+Start reading the string from the last character to the first character.
+Append each character in reverse order.
+
+Step-by-step:
+Read O
+Then L
+Then L
+Then E
+Then H
+
+The reversed string becomes OLLEH.
+
+
+public class Reverse {
+    public static void main(String[] args) {        
+        String str = "ANDREAAAAAAAAAAAA";  
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
+    }
