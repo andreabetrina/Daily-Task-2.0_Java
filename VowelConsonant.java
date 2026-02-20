f26beba18cefef5703c8c8d65f6152986a01f7e4
Andrea Betrina P <andreabetrina06@gmail.com>
2026-02-20 12:38:53 +0530

    Add VowelConsonant.java to check vowels and consonants

---
+++ VowelConsonant.java
@@ -0,0 +1,15 @@
+// to check whether given character is vowel or consonant.
+public class Vowel {
+    public static void main(String[] args) {
+        char ch = 'a';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || 
+            ch == 'o' || ch == 'u' || 
+            ch == 'A' || ch == 'E' || 
+            ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
+```
