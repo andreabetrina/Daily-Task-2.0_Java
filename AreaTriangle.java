dcb55497328eee6bd18efc44e122e23d6e771777
Andrea Betrina P <andreabetrina06@gmail.com>
2026-03-03 23:26:40 +0530

    Create AreaTriangle.java

---
+++ AreaTriangle.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int base = 10;
+        int height = 5;
+        int area = base*height*1/2;
+
+        System.out.println("Area of Triangle " + area);
+    }
+}
